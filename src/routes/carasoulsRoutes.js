44a01f72ef122dabdd96e37e1e7db596b764098d
Neo <dixitrayaan@gmail.com>
2025-06-08 12:12:35 +0530

    laudehfr

--- src/routes/carasoulsRoutes.js
+++ src/routes/carasoulsRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const carasoulController = require('../controllers/carasoulController');
 
 // Route to list all image files for carousels from Bunny.net storage
-router.get('/carasouls', carasoulController.listImages);
+router.get('/', carasoulController.listImages);
 
 module.exports = router;
